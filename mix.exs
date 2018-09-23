1f1b70e223567f3fffa8fc415456d3c6b136e1f0
Maru de Vera <maru@sheepd.com>
2018-09-23 21:33:57 +0800

    Initial Commit

---
+++ mix.exs
@@ -0,0 +1,33 @@
+defmodule WeatherScraper.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :weather_scraper,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:ecto, "~> 2.2.0"},
+      {:httpoison, "~> 1.0"},
+      {:mix_test_watch, "~> 0.8", except: :prod, runtime: false},
+      {:envy, "~> 1.1.1", except: :prod}
+    ]
+  end
+end
