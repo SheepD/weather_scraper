571582d9b6de54b71fd42671817df9f36e77e719
Maru de Vera <maru@sheepd.com>
2018-09-24 08:17:53 +0800

    Add config for ecto as well as repo

--- config/config.exs
+++ config/config.exs
@@ -1,25 +1,13 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# 3rd-party users, it should be done in your "mix.exs" file.
+config :mix_test_watch, clear: true
 
-# You can configure your application as:
-#
-#     config :weather_scraper, key: :value
-#
-# and access this configuration in your application as:
-#
-#     Application.get_env(:weather_scraper, :key)
-#
-# You can also configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
+config :weather_scraper, ecto_repos: [WeatherScraper.Repo]
+
+config :weather_scraper, WeatherScraper.Repo,
+  database: "weather_scraper_dev",
+  hostname: "localhost",
+  pool_size: 10
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
---
+++ lib/weather_scraper/repo.ex
@@ -0,0 +1,5 @@
+defmodule WeatherScraper.Repo do
+  use Ecto.Repo,
+    otp_app: :weather_scraper,
+    adapter: Ecto.Adapters.Postgres
+end
